bfd8c46e300c74e62e7556f48ce6b51998d19cdf
Karan Shah <50394619+karanshah1561998@users.noreply.github.com>
2024-12-09 02:02:08 -0500

    Insert data for customers | bookings | Trips | Payments

---
+++ Insert/Customers.sql
@@ -0,0 +1,202 @@
+INSERT INTO Customers (name, email, phone, address)
+VALUES 
+('Noah Thomas', 'dmartinez@hotmail.com', '3165601046', '33845 Alex Place
+North Allisonmouth, TN 66914'),
+('Laura Barnes', 'timothy38@gmail.com', '2243548799', '3590 Ashley Station
+East Betty, MI 67393'),
+('Ashley Garcia', 'thomasvargas@daniels.com', '5725691280', '5241 Clinton Ville Suite 321
+Smithville, MI 05825'),
+('Robert Whitaker', 'xharris@hotmail.com', '2432584975', '15812 Howard Hills
+South Melvin, NM 80076'),
+('Mark Wright', 'matthewochoa@mcbride.org', '1677738125', 'Unit 5554 Box 9822
+DPO AE 53885'),
+('Alan Douglas', 'uhoffman@baker-patterson.com', '3928734923', '324 Cohen Roads
+Wilsonstad, MT 68122'),
+('Jose Smith', 'tatenicholas@yahoo.com', '1306241939', '680 Maxwell Heights
+Lindabury, AL 75957'),
+('Lauren Rodriguez', 'danielconley@hotmail.com', '5166290788', 'USS Diaz
+FPO AE 19773'),
+('Anna Diaz', 'richardlee@gmail.com', '6366691826', '8991 Brooks Glen Apt. 923
+New Robinside, MA 39082'),
+('Megan Ayala', 'sanchezamy@soto.com', '8857611340', '48950 Winters Drive Suite 209
+New Danielton, CT 71832'),
+('Geoffrey Torres', 'joevasquez@miller.info', '2565370265', '86709 Johnson Route
+Marychester, SC 27292'),
+('Donna Jordan', 'sharon10@jennings.net', '4360843796', '597 Williamson Village Apt. 522
+Port Alanville, WY 32091'),
+('Adam Bautista', 'carlagarcia@mitchell.com', '2596867653', '5773 Hanna Point Suite 282
+New Dominic, MN 71139'),
+('Kevin Carter', 'anthonynorman@gmail.com', '2866811242', '3066 Harrington Views
+Jeremyside, CT 70758'),
+('Joseph Martinez', 'tiffanydavis@lawson-matthews.info', '2445992552', '388 Bowen Passage Apt. 371
+Shannontown, AL 86900'),
+('Sharon Woods', 'pmiller@yahoo.com', '3464984392', '41855 Chelsea Mount
+Englishville, MS 03046'),
+('Robert Miller', 'englishmary@williams.com', '8179168078', '929 Sharon Harbors
+South Ralphton, MN 46845'),
+('William Morse', 'madelinepeterson@cruz.info', '0401964310', 'PSC 2604, Box 8758
+APO AP 63070'),
+('Miss Meagan Barton', 'reedcharles@hotmail.com', '5419276679', '81209 Phillips Extensions Suite 867
+West Stephen, ND 28665'),
+('Brian Jones', 'tammy02@yahoo.com', '4946253410', '93026 Heather Branch Apt. 351
+North Denise, FL 61171'),
+('Cheryl Cooke', 'nathanramirez@shelton.com', '7136919888', '264 Richard Turnpike
+Jamiemouth, NJ 65496'),
+('Trevor Price', 'christopherbond@hotmail.com', '7652817006', '19935 Ellis Spur Suite 791
+North Charlesbury, GA 56304'),
+('Anna Rice', 'littlehannah@hotmail.com', '9254562879', '5802 Nguyen Highway
+Collinstown, OK 71181'),
+('Sarah Lewis', 'cunninghamderek@yahoo.com', '4503419661', '0009 Wyatt Viaduct
+Potterton, ID 94306'),
+('Jeffery Barker', 'smithvincent@rivera.com', '8773881364', '91274 Cruz Union Apt. 368
+Garciamouth, PA 43248'),
+('Timothy Daniels', 'stevenjackson@hotmail.com', '6604500344', '764 Stephen Hills Apt. 904
+Thomasville, GA 04076'),
+('Michael Young', 'bruce10@wright.com', '6132637328', '6559 Tyler Squares Suite 023
+Adamsview, ND 86463'),
+('Scott Daniels', 'fryecynthia@yahoo.com', '8109831865', '230 White Rue
+North Troy, NY 19372'),
+('Robert Nguyen DVM', 'jessicawilliams@garcia.info', '8231016191', '4075 Ana Street
+Jonathanshire, NE 76906'),
+('Thomas Benson', 'ckennedy@smith.biz', '9844019902', '8133 Eric Walk Suite 674
+Jenniferhaven, ME 75245'),
+('Larry Castillo', 'holdercourtney@duarte-ball.com', '9947072875', 'Unit 3953 Box 7139
+DPO AP 04460'),
+('James Mcdaniel', 'cruzwilliam@scott-williamson.biz', '6449370719', '448 Cunningham Mills Suite 426
+East Jonchester, OR 53168'),
+('Kevin Cuevas', 'jorgeserrano@hayes.com', '8853317575', '925 Welch Keys Apt. 442
+Mauriceland, KY 28329'),
+('Michelle Cruz', 'smithdavid@willis.com', '9972580468', 'Unit 4300 Box 4975
+DPO AA 95680'),
+('Michele Lee', 'valerie37@gibbs.org', '2619392619', '59151 Perkins Burgs Suite 351
+Floresbury, AL 52583'),
+('Heather Cole', 'ericglenn@yahoo.com', '3763157122', '06575 Ashley Oval Apt. 906
+Adamsside, HI 50041'),
+('Justin Evans', 'michellefarmer@yahoo.com', '4363155340', '32607 Carl Burg
+East Melindamouth, ID 45811'),
+('Brian Brock', 'munozbarbara@fuller.com', '7797721133', '6579 Debbie Crossing
+New Joanna, CT 61344'),
+('Alexis Ingram', 'powellkyle@guzman-perez.com', '3342674037', '4227 William View Apt. 816
+Wilsonland, NY 69536'),
+('Angela Griffith', 'daniel21@smith-brennan.com', '2540821231', '1762 Cody Spring Suite 124
+Port Jeanetteborough, HI 18554'),
+('Ashley Joseph', 'bridgetsimpson@hotmail.com', '5131754808', '48753 Baker Hill Apt. 828
+Tommymouth, DE 84950'),
+('Wayne Marshall', 'kristopher26@oneill.com', '2641687983', '244 Garcia Neck Suite 071
+Alexandriaville, IN 25488'),
+('Trevor Walker', 'davidwelch@gmail.com', '5642642425', '5427 Danielle Drive Apt. 741
+Williamsland, IA 58892'),
+('Mitchell Acevedo', 'sandrajackson@patel.info', '6280535463', '7975 Cheryl Trafficway Apt. 880
+New Teresa, VT 92931'),
+('Candice Pena', 'hoffmanjames@yahoo.com', '2204300717', '455 Combs Club
+Patrickmouth, HI 27969'),
+('Charles Newton', 'lambertnoah@lawrence.com', '4136289473', '886 Monica Knolls
+New Glenn, AK 18175'),
+('Alex Hamilton', 'kristenjackson@mercado-humphrey.com', '4058729053', '75819 Williamson Circles Apt. 307
+West Jamesshire, NV 06134'),
+('Geoffrey Harrison', 'qbell@bell.biz', '7815052922', '486 Christina Park
+Brandonfort, RI 77113'),
+('Cody Sims', 'stevensondawn@yahoo.com', '0115941049', '40107 Tammy Island Apt. 044
+Sarahview, WA 59158'),
+('April Griffin', 'davidbrown@hotmail.com', '9497703392', '3360 Amanda Lane
+Riveraport, TN 93179'),
+('Jermaine Flores', 'thompsonkaren@hotmail.com', '4157198502', '0561 Orozco Passage
+Katrinaburgh, WI 76515'),
+('Jordan Hopkins', 'daniel92@romero.com', '5878335013', 'PSC 3763, Box 2536
+APO AP 76657'),
+('Roberto Salazar', 'samanthadrake@hurst-clark.com', '4254560341', '393 Johnson Ramp
+Devonmouth, MO 21608'),
+('Daniel Sanchez', 'ypotter@gmail.com', '3809226265', '4827 Alvarez Throughway Apt. 152
+Brianaborough, KY 61426'),
+('Amy Dillon', 'morgandouglas@evans-davis.com', '3928468450', 'Unit 3130 Box 2166
+DPO AE 64408'),
+('Joshua Miller', 'riosjason@carter.com', '9813338173', '423 King Rest
+New Jaime, CA 96608'),
+('Mrs. Tabitha Washington DDS', 'morrissamantha@yahoo.com', '6585445438', '845 Joyce Village Apt. 390
+Owenmouth, MA 07879'),
+('James Kirk', 'dawn08@gmail.com', '3319709030', '370 Barrett Overpass
+Markmouth, VA 39058'),
+('Amber Hull', 'gonzalezkristi@thompson-brown.com', '1450765544', '72048 Grant Ramp Apt. 637
+Timfort, CT 20666'),
+('Julie Mueller', 'dannycastro@leonard-pacheco.com', '5628569215', '87280 Patricia Trafficway
+New Sarahside, UT 26392'),
+('Earl Pitts', 'staceykey@cole-steele.org', '3214792142', '17201 Morris Shoal Apt. 776
+Richardmouth, SC 66561'),
+('Morgan Schmidt', 'edavis@santiago-taylor.com', '0707672012', '81632 Harris Junctions Suite 368
+Amandabury, AL 88412'),
+('Gail Johnson', 'gibsonmaria@hotmail.com', '0533260825', '549 Guzman Springs
+Barnesberg, PA 77211'),
+('Cory Cooper', 'rcordova@hotmail.com', '1458115956', '1166 Williams Green Suite 743
+Sheilahaven, FL 27062'),
+('John Johnson', 'fbradley@yahoo.com', '5339517161', '514 David Street
+Jensenmouth, WV 18673'),
+('Judith Padilla', 'scottbennett@hotmail.com', '9483161571', '5130 Walters Hills
+Cynthiafort, VT 70638'),
+('Jasmine Reid', 'lijose@burke.biz', '9343417697', '3662 Hayes Lakes
+Nicholstown, WA 75107'),
+('David Moore', 'lauraparker@gmail.com', '0795906734', '9296 Richard Junction Apt. 853
+Lake Markview, RI 13467'),
+('Joe Anderson', 'oflores@lindsey-hansen.com', '1200832203', '433 Patterson Mountains
+Reginahaven, NC 12452'),
+('Holly Reynolds', 'twood@allen.net', '1899124662', '4441 Michael Overpass
+Lisahaven, IA 78408'),
+('Gerald Mcfarland', 'ginasmith@fitzgerald-fisher.org', '8721928573', '465 Darlene Oval
+North Pam, PA 26888'),
+('Steven Stanley', 'amanda21@roberts.net', '0610885466', '392 Rhonda Cliff Suite 763
+New Carol, OR 57546'),
+('David Frank', 'nramirez@pittman.biz', '4264478331', '7665 Flores Green Apt. 673
+Blaketon, TX 23587'),
+('Alice Boone', 'petersenjamie@yahoo.com', '2609123253', '9967 Green Freeway
+Garciaburgh, FL 07193'),
+('Jenna Stark', 'dgarcia@thomas.net', '7000563107', '4320 Brock Loaf Suite 213
+Chentown, CT 81458'),
+('Christina Smith', 'christopherdyer@riddle.com', '5573683628', '53735 Evans Court Apt. 434
+North Lynnmouth, RI 17953'),
+('Benjamin Gibson', 'millerpaul@marshall-neal.org', '4641395815', '0308 Barbara Lights
+Charlesville, NM 53671'),
+('Ashley Young', 'jessica44@gmail.com', '9393423788', '59659 Mccoy Via Apt. 505
+Meganmouth, DC 21736'),
+('Charles Hayes', 'msandoval@gmail.com', '9110604018', '7601 Ramos Drives Suite 337
+Thomasborough, SD 37531'),
+('Mario Gordon', 'tamara39@hotmail.com', '1764312231', '782 Todd Islands
+North Jessica, MI 54561'),
+('Courtney Steele', 'triggs@hoover.com', '6939265281', '85864 Cynthia Pass
+West Amanda, CA 72080'),
+('Sarah Washington', 'zreyes@henry.com', '6148280599', '98709 Aaron Ridges Apt. 061
+Davisstad, HI 67888'),
+('Michael Roberts PhD', 'hstevens@gmail.com', '4271725000', '87802 Brown Crescent Apt. 554
+East Jodi, NV 42398'),
+('Miranda Rogers', 'melissagriffith@hotmail.com', '5518595506', 'PSC 3696, Box 4272
+APO AP 73957'),
+('Stacy Sawyer', 'brandonhernandez@white.com', '0156910320', '31264 Ross Shoal
+New Jessemouth, MI 58156'),
+('Donald Gomez', 'rgomez@phillips-savage.biz', '9169804661', '580 Deborah Flats
+Jennabury, KY 78033'),
+('Robert Ramos', 'jasonramos@thompson.com', '4009881198', '651 Mckinney Plaza
+Matthewshire, NH 05511'),
+('David Medina', 'nortonkaitlyn@garrett.com', '6663319819', '365 John Bridge
+Charlesport, WV 78366'),
+('Kevin Stevens', 'smithchristopher@hotmail.com', '8489262654', '1529 Rebecca Course Suite 405
+Robertborough, NC 52191'),
+('Colin Morrison', 'sydneymcdaniel@yoder.org', '3000026429', '625 Holly Orchard
+Port Michaelmouth, MS 27275'),
+('Charles Horn', 'ricky19@hotmail.com', '7642399442', '841 Rivera Rapids Apt. 276
+Kendraborough, AK 13881'),
+('Sabrina Macdonald', 'daniel64@harrington.org', '2326896290', '119 Corey Falls Suite 677
+Kathrynside, NH 62296'),
+('Christopher Lowe', 'bgonzalez@riley.biz', '4416240015', '1312 Catherine Courts
+West Peterbury, WI 11657'),
+('Jill Salazar', 'wmason@robertson.com', '5143956368', '381 Garcia Courts
+Robertberg, SC 01344'),
+('Amy Marsh', 'michele71@gmail.com', '3293592631', '678 Courtney Loop
+East Annebury, MN 42889'),
+('Daniel Craig', 'phillip62@davis.info', '0349949894', '3104 Mark Fork Suite 539
+Timothybury, TN 98195'),
+('Javier Turner', 'gregory04@woods.org', '7653554706', '25002 Kelly Throughway
+Johnland, GA 25258'),
+('Steven Ward', 'wellsjennifer@gmail.com', '8728275157', '61270 David Lock Apt. 439
+Aaronside, MI 50240'),
+('Eric Skinner', 'clarson@yahoo.com', '1015401550', '7620 Raymond Meadow
+New Gary, OK 56184'),
+('Audrey Hall', 'ltodd@elliott.com', '9559178141', '9629 Mccormick Fork
+Lake Georgeview, MD 69958');
